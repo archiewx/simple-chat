412fdb9ea4a77f374900a8c384bfd92b70f827cf
triste <zhenglfsir@gmail.com>
2017-08-27 14:12:29 +0800

    增加oss 上传示例[待完成】

---
+++ src/other/components/app.vue
@@ -0,0 +1,22 @@
+<!--
+ *  creator: zheng
+ *  date: 2017/8/23
+ *  email: zhenglfsir@gmail.com
+ *  desc:
+ -->
+<template>
+    <div>
+      <p>当前访问到了other.html</p>
+    </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
---
+++ src/other/main.js
@@ -0,0 +1,14 @@
+/**
+ *  creator: zheng
+ *  date: 2017/8/23
+ *  email: zhenglfsir@gmail.com
+ */
+import Vue from 'vue'
+import app from './components/app.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<app/>',
+  components: {app}
+})
